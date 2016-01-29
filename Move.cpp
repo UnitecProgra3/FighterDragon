545a97a158355c3f9325a0aa1dad582e79061b8b
Turupawn <ahmed.hn.43@gmail.com>
2016-01-29 11:25:52 -0600

    primer commit

---
+++ Move.cpp
@@ -0,0 +1,38 @@
+#include "Move.h"
+
+Move::Move(SDL_Renderer* renderer)
+{
+    this->renderer=renderer;
+    Sprite *sprite1=new Sprite(renderer,"assets/idle/1.png",75);
+    Sprite *sprite2=new Sprite(renderer,"assets/idle/2.png",75);
+    Sprite *sprite3=new Sprite(renderer,"assets/idle/3.png",75);
+    Sprite *sprite4=new Sprite(renderer,"assets/idle/4.png",75);
+    sprites.push_back(sprite1);
+    sprites.push_back(sprite2);
+    sprites.push_back(sprite3);
+    sprites.push_back(sprite4);
+    current_sprite=0;
+    frame=0;
+    current_sprite_frame=0;
+}
+
+Move::~Move()
+{
+    //dtor
+}
+
+void Move::draw()
+{
+    Sprite* sprite = sprites[current_sprite];
+    sprite->draw();
+    current_sprite_frame++;
+    if(current_sprite_frame>=sprite->frames)
+    {
+        current_sprite++;
+        if(current_sprite>=sprites.size())
+            current_sprite=0;
+        current_sprite_frame=0;
+    }
+
+    frame++;
+}
---
+++ main.cpp
@@ -0,0 +1,79 @@
+
+#include<SDL2/SDL.h>
+#include<SDL2/SDL_image.h>
+#include<iostream>
+#include "Move.h"
+
+using namespace std;
+
+SDL_Window* window;
+SDL_Renderer* renderer;
+SDL_Event Event;
+SDL_Texture *background,*character;
+SDL_Rect rect_background,rect_character;
+
+
+
+int main( int argc, char* args[] )
+{
+    //Init SDL
+    if(SDL_Init(SDL_INIT_EVERYTHING) < 0)
+    {
+        return 10;
+    }
+    //Creates a SDL Window
+    if((window = SDL_CreateWindow("Image Loading", 100, 100, 500/*WIDTH*/, 250/*HEIGHT*/, SDL_WINDOW_RESIZABLE | SDL_RENDERER_PRESENTVSYNC)) == NULL)
+    {
+        return 20;
+    }
+    //SDL Renderer
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED );
+    if (renderer == NULL)
+    {
+        std::cout << SDL_GetError() << std::endl;
+        return 30;
+    }
+
+    //Init textures
+    background = IMG_LoadTexture(renderer,"fondo.png");
+    rect_background.x = 0;
+    rect_background.y = 0;
+    rect_background.w = 500;
+    rect_background.h = 250;
+
+    character = IMG_LoadTexture(renderer, "personaje.png");
+    rect_character.x = 0;
+    rect_character.y = 100;
+    rect_character.w = 32;
+    rect_character.h = 32;
+
+    Move move(renderer);
+
+    //Main Loop
+    while(true)
+    {
+        while(SDL_PollEvent(&Event))
+        {
+            if(Event.type == SDL_QUIT)
+            {
+                return 0;
+            }
+            if(Event.type == SDL_KEYDOWN)
+            {
+                if(Event.key.keysym.sym == SDLK_d)
+                    rect_character.x++;
+                if(Event.key.keysym.sym == SDLK_a)
+                    rect_character.x--;
+            }
+        }
+
+        SDL_RenderCopy(renderer, background, NULL, &rect_background);
+        SDL_RenderCopy(renderer, character, NULL, &rect_character);
+
+        move.draw();
+
+        SDL_RenderPresent(renderer);
+    }
+
+	return 0;
+}
